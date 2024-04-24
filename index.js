5fea9849f5576020ddaa236850c4ce64f89932a2
kunxin-chor <chorkunxin@yahoo.com>
2024-04-24 02:19:06 +0000

    removed boilerplate code

--- index.js
+++ index.js
@@ -22,24 +22,7 @@ async function main() {
 
     const connection = getConnection();
 
-    app.get('/api/customers', async function (req, res) {
-        const [customers] = await connection.execute("SELECT * FROM Customers");
-        res.json({
-            'customers': customers
-        })
-    });
-
-    app.post('/api/customers', async function (req, res) {
-        // we can extract the data from req.body
-        // (extract out each key's value as a variable use destructuring)
-        const { first_name, last_name, rating, company_id } = req.body;
-        const [results] =await connection.execute(`
-            INSERT INTO Customers (first_name, last_name, rating, company_id)
-            VALUES (?,?,?,?)
-        `, [first_name, last_name, rating, company_id]);
-        res.json(results);
-
-    })
+    
 }
 
 main();
